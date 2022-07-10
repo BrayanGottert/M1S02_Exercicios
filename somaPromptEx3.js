445f535e2724c25bcdab53a101ffa344598180f1
Brayan Göttert <ibgottert@gmail.com>
2022-07-10 20:09:56 -0300

    thrid commit

---
+++ somaPromptEx3.js
@@ -0,0 +1,4 @@
+var num1Prompt = window.prompt("Digite o primeiro número a ser somado: ");
+var num2Prompt = window.prompt("Digite o último número a ser somado: ");
+var sumPrompt = parseInt(num1Prompt) + parseInt(num2Prompt);
+document.write("<h1>" + sumPrompt + "</h1>")
