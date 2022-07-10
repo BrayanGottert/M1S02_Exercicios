7822a85a9f2b8ad104638993fc55676f7f356761
Brayan Göttert <ibgottert@gmail.com>
2022-07-10 20:31:38 -0300

    fifth commit

---
+++ condPrintEx5.js
@@ -0,0 +1,7 @@
+var numTest = window.prompt("Digite um número: ");
+if (parseInt(numTest) %2 == 0){
+   document.write("O seu núemro é par!");
+}
+else {
+    document.write("O seu número é ímpar!");
+}
